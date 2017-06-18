d5eed6ac79c3b44fd4c5d7ecac994ef8f95dad1a
Allan M <allanmukundi@hotmail.com>
2017-06-18 18:05:41 -0400

    Added game assets.

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 'use strict'
 
 var port     =      process.env.PORT || 8000;
-var app      =      require('express')();
+var express  =      require('express');
+var app      =      express();
 var server   =      require('http').Server(app);
 var io       =      require('socket.io')(server);
 
+// Start the server
 server.listen(port);
 console.log('Listening on port %d in %s mode.',
-            server.address().port, app.settings.env)
+            server.address().port, app.settings.env);
+
+app.use(express.static(__dirname + '/static'));
+
+// Serve index.html at root
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
